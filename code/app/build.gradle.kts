1b15747eb26954391be61d651d8da7fed635bcf2
Kabirvansh <157079236+Kabirvansh@users.noreply.github.com>
2025-03-09 11:43:21 -0600

    testing profileactivity

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.0.5")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
