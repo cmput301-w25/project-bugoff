1fd517ee9b4793c6817679a223ddd4282e1d5dde
Bhavjot Singh <109903185+thebhavjotsingh@users.noreply.github.com>
2025-03-28 02:47:28 -0600

    Update MoodAdapter.java

--- code/app/src/main/java/com/example/whimsy/MoodAdapter.java
+++ code/app/src/main/java/com/example/whimsy/MoodAdapter.java
@@ -15,7 +15,6 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
