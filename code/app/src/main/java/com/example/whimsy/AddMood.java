99fb60e639de07473fcfef7f7176275c38b51d57
Bhavjot Singh <109903185+thebhavjotsingh@users.noreply.github.com>
2025-03-31 16:02:40 -0600

    Changes made

--- code/app/src/main/java/com/example/whimsy/AddMood.java
+++ code/app/src/main/java/com/example/whimsy/AddMood.java
@@ -64,8 +64,6 @@ import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.core.app.ActivityCompat;
@@ -74,9 +72,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.target.Target;
-import com.bumptech.glide.request.transition.Transition;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
